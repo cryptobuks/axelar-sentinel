8a4f77c9efc3c896cfb3d2051cec19c456ec5d99
João Sousa <joaosousa@MBP-de-Joao.home>
2023-01-18 03:00:00 +0000

    add more protobufs

--- build.rs
+++ build.rs
@@ -2,9 +2,14 @@ use std::io::Result;
 fn main() -> Result<()> {
     prost_build::compile_protos(
         &[
-            "axelar-core/proto/axelar/vote/v1beta1/tx.proto",
+            "axelar-core/proto/axelar/multisig/exported/v1beta1/types.proto",
+            "axelar-core/proto/axelar/multisig/v1beta1/events.proto",
+            "axelar-core/proto/axelar/multisig/v1beta1/tx.proto",
+            "axelar-core/proto/axelar/multisig/v1beta1/types.proto",
             "axelar-core/proto/axelar/vote/exported/v1beta1/types.proto",
-            "axelar-core/proto/axelar/permission/exported/v1beta1/types.proto",
+            "axelar-core/proto/axelar/vote/v1beta1/tx.proto",
+            "axelar-core/proto/axelar/evm/v1beta1/events.proto",
+            "axelar-core/proto/axelar/evm/v1beta1/types.proto",
             "proto/tofnd/grpc.proto",
             "proto/tofnd/multisig.proto",
         ],
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,15 @@ pub mod tm_client;
 pub mod tofnd {
     include!(concat!(env!("OUT_DIR"), "/tofnd.rs"));
 }
+
+pub mod cosmos {
+    pub mod base {
+        pub mod v1beta1 {
+            include!(concat!(env!("OUT_DIR"), "/cosmos.base.v1beta1.rs"));
+        }
+    }
+}
+
 pub mod axelar {
     pub mod utils {
         pub mod v1beta1 {
@@ -20,6 +29,21 @@ pub mod axelar {
             pub mod v1beta1 {
                 include!(concat!(env!("OUT_DIR"), "/axelar.snapshot.exported.v1beta1.rs"));
             }
+        }
+    }
+    pub mod multisig {
+        pub mod exported {
+            pub mod v1beta1 {
+                include!(concat!(env!("OUT_DIR"), "/axelar.multisig.exported.v1beta1.rs"));
+            }
+        }
+        pub mod v1beta1 {
+            include!(concat!(env!("OUT_DIR"), "/axelar.multisig.v1beta1.rs"));
+        }
+    }    
+    pub mod evm {
+        pub mod v1beta1 {
+            include!(concat!(env!("OUT_DIR"), "/axelar.evm.v1beta1.rs"));
         }
     }
     pub mod vote {
