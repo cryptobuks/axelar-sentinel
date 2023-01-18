69f64a4dae9b0dcf26feb0dded74e3e46382e4df
João Sousa <joaosousa@MBP-de-Joao.home>
2023-01-18 00:01:03 +0000

    format

--- build.rs
+++ build.rs
@@ -1,18 +1,19 @@
 use std::io::Result;
 fn main() -> Result<()> {
-    prost_build::compile_protos(&[
+    prost_build::compile_protos(
+        &[
             "proto/axelar/vote/v1beta1/tx.proto",
             "proto/axelar/vote/exported/v1beta1/types.proto",
             "proto/axelar/permission/exported/v1beta1/types.proto",
             "proto/tofnd/grpc.proto",
-            "proto/tofnd/multisig.proto"
+            "proto/tofnd/multisig.proto",
         ],
         &[
             "third_party/cosmos-sdk/proto/",
             "third_party/tendermint/proto/",
             "third_party/proto/",
             "proto/tofnd/",
-            "proto/"
+            "proto/",
         ],
     )?;
     Ok(())
